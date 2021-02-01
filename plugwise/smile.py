ccbe62a54eeb248bc150465a5a7f8e23f6a9f28f
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2021-02-01 19:17:04 +0100

    Don't add fw-info when None

--- plugwise/smile.py
+++ plugwise/smile.py
@@ -494,6 +494,8 @@ class Smile:
                 "class": appliance_class,
                 "location": appliance_location,
             }
+            if appliance_fw is None:
+                appliances[appliance_id].pop("fw", None)
 
         return appliances
 
