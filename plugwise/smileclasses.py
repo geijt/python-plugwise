123f513e0ea8d81d5028976d1e463ff4e5cede68
Bouwe <bouwe.s.westerdijk@gmail.com>
2021-03-23 20:25:36 +0100

    Add file smileclasses.py

---
+++ plugwise/smileclasses.py
@@ -0,0 +1,159 @@
+from homeassistant.components.climate.const import (
+    CURRENT_HVAC_COOL,
+    CURRENT_HVAC_HEAT,
+    CURRENT_HVAC_IDLE,
+    HVAC_MODE_AUTO,
+    HVAC_MODE_HEAT,
+    HVAC_MODE_HEAT_COOL,
+    HVAC_MODE_OFF,
+    PRESET_AWAY,
+)
+
+HVAC_MODES_HEAT_ONLY = [HVAC_MODE_HEAT, HVAC_MODE_AUTO, HVAC_MODE_OFF]
+HVAC_MODES_HEAT_COOL = [HVAC_MODE_HEAT_COOL, HVAC_MODE_AUTO, HVAC_MODE_OFF]
+
+class SmileThermostat:
+    """Represents a Smile Thermostat."""
+
+    def __init__(self, api, dev_id):
+        """Initialize the paramaters."""
+        self._api = api
+        self._dev_id = dev_id
+        self._extra_state_attributes = None
+        self._hvac_action = None
+        self._hvac_mode = None
+        self._hvac_modes = None
+        self._get_presets = None
+        self._preset_mode = None
+        self._preset_modes = None
+        self._current_temperature = None
+        self._schema_names = None
+        self._selected_schema = None
+        self._target_temperature = None
+
+        self._compressor_state = None
+        self._cooling_state = None
+        self._heating_state = None
+        self._schema_status = None
+        self._single_thermostat = None
+
+        self._active_device = self._api.active_device_present
+        self._heater_id = self._api.heater_id
+        self._single_thermostat = self._api.single_master_thermostat()
+
+        self.update_data()
+
+    @property
+    def hvac_action(self):
+        """HVAC action."""
+        return self._hvac_action
+
+    @property
+    def hvac_mode(self):
+        """Active HVAC mode."""
+        return self._hvac_mode
+
+    @property
+    def hvac_modes(self):
+        """Available HVAC modes."""
+        return self._hvac_modes
+
+    @property
+    def preset_mode(self):
+        """Active preset mode."""
+        return self._preset_mode
+
+    @property
+    def preset_modes(self):
+        """Available preset modes."""
+        return self._preset_modes
+
+    @property
+    def current_temperature(self):
+        """Current measured temperature."""
+        return self._current_temperature
+
+    @property
+    def target_temperature(self):
+        """Target temperature."""
+        return self._target_temperature
+
+    @property
+    def extra_state_attributes(self):
+        """Extra state attributes."""
+        return self._extra_state_attributes
+
+    def update_data(self):
+        """Handle update callbacks."""
+        #_LOGGER.debug("Processing data from device %d", self._dev_id)
+        
+        climate_data = self._api.get_device_data(self._dev_id)
+        if self._active_device:
+            heater_central_data = self._api.get_device_data(self._heater_id)
+            self._compressor_state = heater_central_data.get("compressor_state")
+            self._cooling_state = heater_central_data.get("cooling_state")
+            self._heating_state = heater_central_data.get("heating_state")
+
+        # current & target_temps
+        self._target_temperature = climate_data.get("setpoint")
+        self._current_temperature = climate_data.get("temperature")
+
+        # hvac action
+        if self._single_thermostat:
+            if self._heating_state:
+                self._hvac_action = CURRENT_HVAC_HEAT
+            if self._cooling_state:
+                self._hvac_action = CURRENT_HVAC_COOL
+            self._hvac_action = CURRENT_HVAC_IDLE
+
+        elif self._target_temperature > self._current_temperature:
+            self._hvac_action = CURRENT_HVAC_HEAT
+
+        else:
+            self._hvac_action = CURRENT_HVAC_IDLE
+
+        # hvac mode
+        self._hvac_mode = HVAC_MODE_AUTO
+        if "selected_schedule" in climate_data:
+            self._selected_schema = climate_data["selected_schedule"]
+            self._schema_status = False
+            if self._selected_schema is not None:
+                self._schema_status = True
+
+        if not self._schema_status:
+            if self._preset_mode == PRESET_AWAY:
+                self._hvac_mode = HVAC_MODE_OFF
+            else:
+                self._hvac_mode = HVAC_MODE_HEAT
+                if self._compressor_state is not None:
+                    self._hvac_mode = HVAC_MODE_HEAT_COOL
+
+        # hvac modes
+        if self._compressor_state is not None:
+            self._hvac_mode = HVAC_MODES_HEAT_COOL
+        else:
+            self._hvac_mode = HVAC_MODES_HEAT_ONLY
+
+        # preset modes
+        self._get_presets = climate_data.get("presets")
+        if self._get_presets:
+            self._preset_modes = list(self._get_presets)
+
+        # preset mode
+        self._preset_mode = climate_data.get("active_preset")
+
+        # extra state attributes
+        attributes = {}
+        self._schema_names = climate_data.get("available_schedules")
+        self._selected_schema = climate_data.get("selected_schedule")
+        if self._schema_names:
+            attributes["available_schemas"] = self._schema_names
+        if self._selected_schema:
+            attributes["selected_schema"] = self._selected_schema
+        self._extra_state_attributes = attributes
+
+
+
+        
+
+    
