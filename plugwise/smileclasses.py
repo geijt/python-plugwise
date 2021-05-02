7510258ca6793f476b90f426474af67194185998
Bouwe <bouwe.s.westerdijk@gmail.com>
2021-05-02 10:15:52 +0200

    Add P1 Class

--- plugwise/smileclasses.py
+++ plugwise/smileclasses.py
@@ -387,3 +387,53 @@ class Plug:
             for key, value in switch.items():
                 if data.get(value[ATTR_ID]) is not None:
                     self.switches[key][ATTR_STATE] = data.get(value[ATTR_ID])
+
+
+class P1:
+    """ Represent the Plugwise P1 device."""
+
+    def __init__(self, api, dev_id):
+        """Initialize the P1."""
+        self._api = api
+        self._dev_id = dev_id
+
+        self.sensors = {}
+
+        self.sensor_list = [
+            EL_CONSUMED_PEAK_INTERVAL,
+            EL_CONSUMED_OFF_PEAK_INTERVAL,
+            EL_CONSUMED_OFF_PEAK_POINT,
+            EL_CONSUMED_PEAK_POINT,
+            EL_CONSUMED_OFF_PEAK_CUMULATIVE,
+            EL_CONSUMED_PEAK_CUMULATIVE,
+            EL_PRODUCED_PEAK_INTERVAL,
+            EL_PRODUCED_OFF_PEAK_INTERVAL,
+            EL_PRODUCED_OFF_PEAK_POINT,
+            EL_PRODUCED_PEAK_POINT,
+            EL_PRODUCED_OFF_PEAK_CUMULATIVE,
+            EL_PRODUCED_PEAK_CUMULATIVE,
+            NET_EL_POINT,
+            NET_EL_CUMULATIVE,
+            GAS_CONSUMED_INTERVAL,
+            GAS_CONSUMED_CUMULATIVE,
+        ]
+
+        self.init_data()
+
+    def init_data(self):
+        """Collect the initial data."""
+        data = self._api.get_device_data(self._dev_id)
+
+        for sensor in self.sensor_list:
+            for key, value in sensor.items():
+                if data.get(value[ATTR_ID]) is not None:
+                    self.sensors.update(sensor)
+
+    def update_data(self):
+        """Handle update callbacks."""
+        data = self._api.get_device_data(self._dev_id)
+
+        for sensor in self.sensor_list:
+            for key, value in sensor.items():
+                if data.get(value[ATTR_ID]) is not None:
+                    self.sensors[key][ATTR_STATE] = data.get(value[ATTR_ID])
