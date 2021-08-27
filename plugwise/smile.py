44ff2dc01ebfc5877ee7dd589bda3da5028a90a5
autoblack <plugwise@users.noreply.github.com>
2021-08-27 14:52:09 +0000

    fixup: output_to_duc Python code reformatted using Black

--- plugwise/smile.py
+++ plugwise/smile.py
@@ -227,7 +227,7 @@ class Smile(SmileHelper):
             self._appliances = await self._request(APPLIANCES)
 
         self.gw_data["notifications"] = self.notifications
-        
+
         for dev_id, dev_dict in self.gw_devices.items():
             data = self._get_device_data(dev_id)
             for key, value in list(data.items()):
